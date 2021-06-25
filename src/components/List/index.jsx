733991f16e686cda96dc06f2ecd337a46b624aa0
EossForos <volosonopulos@gmail.com>
2021-06-25 16:25:03 +0300

    state in addList/index

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import classNames from 'classnames'
 
 import "./list.scss"
 
-const List = ({ items }) => {
+const List = ({ items, isRemovable }) => {
     return <ul className="list">
-        {items.map(item => (
-            <li class={item.active ? 'active': ''}>
+        {items.map((item, index) => (
+            <li key={index} className={classNames(item.className, {active: item.active})}>
                 <i>
                     {item.icon ? (
                         item.icon
